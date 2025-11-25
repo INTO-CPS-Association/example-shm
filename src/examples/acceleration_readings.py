f9b3e391eab276347a3ecb07c915cac48b17d71e
au650680 <au650680@uni.au.dk>
2025-11-25 15:19:45 +0100

    Revert back to "sysid" key in configurations
    - Changed key from "MQTT" back to "sysid" for configuration and code

--- src/examples/acceleration_readings.py
+++ src/examples/acceleration_readings.py
@@ -4,7 +4,7 @@ from data.comm.mqtt import setup_mqtt_client, load_config, shutdown  # type: ign
 
 def read_accelerometers(config_path):
     config = load_config(config_path)
-    sysid_config = config["MQTT"]
+    sysid_config = config["sysid"]
     mqtt_client = setup_mqtt_client(sysid_config, sysid_config['TopicsToSubscribe'][0])
     mqtt_client.connect(sysid_config["host"], sysid_config["port"], 60)
     mqtt_client.loop_start()
--- src/pt_mock/publish_samples.py
+++ src/pt_mock/publish_samples.py
@@ -196,7 +196,7 @@ def main(config_path: str = "config/R-PI.json",
          offset_path: str = "config/offset.json",
          run_once: bool = False) -> None:
     config = load_config(config_path)
-    mqtt_config = config["MQTT"]
+    mqtt_config = config["sysid"]
     mqtt_client, _, _ = setup_mqtt_client(mqtt_config)
 
     mqtt_client.connect(mqtt_config["host"], mqtt_config["port"], 60)
