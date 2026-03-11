270dc7b25b46b44968e36a80d386346953e82684
au650680 <au650680@uni.au.dk>
2026-03-11 14:27:40 +0100

    Removed os pathing from MU

--- src/methods/model_update.py
+++ src/methods/model_update.py
@@ -1,10 +1,10 @@
 import datetime
-import os
 import threading
 import json
 from typing import Any, List, Dict, Optional, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
+from pathlib import Path
 from paho.mqtt.client import Client as MQTTClient, MQTTMessage
 from data.comm.mqtt import (shutdown,start_mqtt, publish_to_mqtt)
 from functions.util import (convert_numpy_to_list, _convert_list_to_dict_or_array)
@@ -169,7 +169,7 @@ def save_model_parameters(config: Dict[str,Any], timestamp: str,
     """
     if model_parameters is not None:
         # Ensure output directory exists
-        os.makedirs(MODEL_DIR, exist_ok=True)
+        Path(MODEL_DIR).mkdir(parents=True, exist_ok=True)
 
         # Thread-safe file locks
         file_locks = {topic: threading.Lock() for topic in config["model_update"]["TopicsToSubscribe"]}
@@ -178,7 +178,7 @@ def save_model_parameters(config: Dict[str,Any], timestamp: str,
             "timestamp": timestamp,
             "parameters": convert_numpy_to_list(model_parameters)
         }
-        file_path = os.path.join(MODEL_DIR, MODEL_PARS_NAME)
+        file_path = Path(MODEL_DIR)/MODEL_PARS_NAME
         with file_locks[config["model_update"]["TopicsToSubscribe"][0]]:
             with open(file_path, "a", encoding="utf-8") as f:
                 f.write(json.dumps(record) + "\n")
@@ -197,14 +197,14 @@ def load_model_parameters() -> Optional[Tuple[str, Dict[str,Any]]]:
 
     """
     try:
-        path = os.path.join(MODEL_DIR, MODEL_PARS_NAME)
-        if not os.path.exists(path):
+        path = Path(MODEL_DIR) / MODEL_PARS_NAME
+        if not path.exists():
             print(f"File not found: {path}. Proceed with standard parameters.")
             model_parameters = MODEL_PARAMETERS
             timestamp = datetime.datetime.now(datetime.timezone.utc).isoformat()
             return timestamp, model_parameters
         else:
-            with open(path, 'r') as json_file:
+            with path.open('r') as json_file:
                 data = json.loads(json_file.readlines()[-1])
             timestamp = data['timestamp']
             model_parameters = data['parameters']
@@ -268,7 +268,6 @@ def live_model_update_with_remote_clustering(config: Dict[str,Any],
                     fig_axes = model_update_plots([1,1], model_parameters,
                                                   params['pars_to_update'], omega_model,
                                                  fig_axes)
-
     except KeyboardInterrupt:
         print("Keyboard interrupt of live model updating\n")
     except Exception as e:
