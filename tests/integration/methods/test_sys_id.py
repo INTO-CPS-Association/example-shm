24d221367f10000b812f19c6411370818cc1d552
au650680 <au650680@uni.au.dk>
2026-03-16 08:45:02 +0100

    Increased covariance threshold in sysid test

--- tests/integration/methods/test_sys_id.py
+++ tests/integration/methods/test_sys_id.py
@@ -39,10 +39,10 @@ def test_sysid():
     stored_cov_damping = stored_data['cov_damping']
     stored_mode_shapes = stored_data['mode_shapes']
     
-
     tolerance = 0.4
+    cov_tolerance = 10
     assert np.allclose(frequencies, stored_frequencies, atol=tolerance, equal_nan=True), "Frequencies do not match!"
-    assert np.allclose(cov_freq, stored_cov_freq, atol=tolerance, equal_nan=True), "Covariance frequencies do not match!"
+    assert np.allclose(cov_freq, stored_cov_freq, atol=cov_tolerance, equal_nan=True), "Covariance frequencies do not match!"
     assert np.allclose(damping_ratios, stored_damping_ratios, atol=tolerance, equal_nan=True), "Damping ratios do not match!"
     assert np.allclose(cov_damping, stored_cov_damping, atol=tolerance*2, equal_nan=True), "Covariance damping ratios do not match!"
     assert np.allclose(mode_shapes, stored_mode_shapes, atol=tolerance, equal_nan=True), "Mode shapes do not match!"
